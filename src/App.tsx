d59e7b131d3dbfa646cdea1f3a894ee7b1a122c8
jonpena <gran_200014@hotmail.com>
2025-09-06 16:31:52 +0200

    refactor: code structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,28 @@
-import { Sidebar } from "./components/Sidebar";
-import { MainContent } from "./components/MainContent";
-import { useRef } from "react";
-import { PrintButton } from "./components/buttons/PrintButton";
-import LanguageButton from "./components/buttons/LanguageButton";
-import { EditButton } from "./components/buttons/EditButton";
+import { Sidebar } from './components/Sidebar';
+import { MainContent } from './components/MainContent';
+import { useRef } from 'react';
+import { PrintButton } from './components/buttons/PrintButton';
+import LanguageButton from './components/buttons/LanguageButton';
+import { EditButton } from './components/buttons/EditButton';
+import { TranslationProvider } from './context/TranslationContext';
 
 const App = () => {
-  const contentRef = useRef<HTMLDivElement>(null);
+  const contentRef = useRef<HTMLDivElement>(null!);
 
   return (
-    <>
-      <div className="flex gap-x-0.5 fixed top-2 right-2 print:!hidden">
+    <TranslationProvider>
+      <div className='flex gap-x-0.5 fixed top-2 right-2 print:!hidden'>
         <PrintButton contentRef={contentRef} />
         <LanguageButton />
         <EditButton />
       </div>
-      <div
-        ref={contentRef}
-        className="min-h-screen bg-gray-100 p-0 print:p-0 lg:p-8"
-      >
-        <div className="cv-container max-w-[297mm] mx-auto grid grid-cols-1 md:grid-cols-[400px_1fr] shadow-2xl rounded-sm print:rounded-none overflow-hidden">
+      <div ref={contentRef} className='min-h-screen bg-gray-100 p-0 print:p-0 lg:p-8'>
+        <div className='cv-container max-w-[297mm] mx-auto grid grid-cols-1 md:grid-cols-[400px_1fr] shadow-2xl rounded-sm print:rounded-none overflow-hidden'>
           <Sidebar />
           <MainContent />
         </div>
       </div>
-    </>
+    </TranslationProvider>
   );
 };
 
